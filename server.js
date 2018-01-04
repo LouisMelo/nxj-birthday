11f34f72f2d8e1768ff30afd8247df73b8f849d1
louis <louis@promote.cache-dns.local>
2018-01-04 15:47:26 +0800

    simple chat test

--- server.js
+++ server.js
@@ -1,63 +1,29 @@
 'use strict';
 
 const express = require('express');
-const socketIO = require('socket.io');
-const path = require('path');
-const mongo = require('mongodb').MongoClient;
+var app = express();
 
 const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, 'index.html');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
 
-const io = socketIO(server);
+io.on('connection', function(socket){
+    console.log('a user connected');
 
-// connect to mongodb
+    socket.on('chat message', function(msg){
+        // console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
 
-mongo.connect('mongodb://louis:louisnow117@ds115085.mlab.com:15085/louismongo', function(err, db){
-    if(err){
-        throw err;
-    }
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+});
 
-    console.log('mongodb connected...')
-
-    io.on('connection', function(socket){
-        console.log('client conected...');
-
-        let chat = db.collection('nxj_birthday');
-
-        chat.find().limit(100).sort({_id:1}).toArray(function(err, res){
-            if(err){
-                throw err;
-            }
-
-            socket.emit('output', res);
-        });
-
-        socket.on('input', function(data){
-            let name = data.name;
-            let message = data.message;
-
-            if(name == '' || message == ''){
-                console.log('Please enter a name and message.');
-            } else {
-                chat.insert({name: name, message: message}, function(){
-                    io.emit('output', [data]);
-                });
-            }
-        });
-
-
-
-        socket.on('disconnected', () => console.log('client disconnected...'));
-    })
-})
-
-// io.on('connection', (socket) => {
-//   console.log('Client connected');
-//   socket.on('disconnect', () => console.log('Client disconnected'));
-// });
-
-// setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+http.listen(PORT, function () {
+    console.log('magic happens in: ' + PORT);
+});
